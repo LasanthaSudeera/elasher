318997cd511f7f4aed29ed439001b2d2937b83f8
Lasantha <lasan.sudeera@gmail.com>
2022-11-06 20:51:06 +0530

    EL-3: Basic dynamic sidebar functional

---
+++ config/sidebar.php
@@ -0,0 +1,51 @@
+<?php
+return [
+    [
+        'menu_header' => 'Testing Dashboard',
+        'menu' => [
+            [
+                'name' => 'Testing Dashboard',
+                'icon_class' => 'fas fa-fire',
+                'submenu' => [
+                    [
+                        'name' => 'General Dashboard',
+                        'url' => 'home'
+                    ],
+                    [
+                        'name' => 'E-Commerce Dashboard',
+                        'url' => 'home'
+                    ]
+                ]
+            ],
+            [
+                'name' => 'Testing Dashboard',
+                'icon_class' => 'fas fa-fire',
+                'submenu' => [
+                    [
+                        'name' => 'General Dashboard',
+                        'url' => 'home'
+                    ],
+                    [
+                        'name' => 'E-Commerce Dashboard',
+                        'url' => 'home'
+                    ]
+                ]
+            ],
+            [
+                'name' => 'Testing Blank Page',
+                'icon_class' => 'far fa-square',
+                'url' => 'home'
+            ]
+        ]
+    ],
+    [
+        'menu_header' => 'Testing Bank Page',
+        'menu' => [
+            [
+                'name' => 'Testing Blank Page',
+                'icon_class' => 'far fa-square',
+                'url' => 'home'
+            ]
+        ]
+    ]
+];
