431495498854746c2ad0a6502b34afc94be710b7
Lasantha <lasan.sudeera@gmail.com>
2022-11-06 19:45:46 +0530

    EL-3: Added breadcrumbs placeholder

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,7 +29,12 @@
             <div class="main-content">
                 <section class="section">
                     <div class="section-header">
-                        <h1>Blank Page</h1>
+                        <h1>Breadcrumb</h1>
+                        <div class="section-header-breadcrumb">
+                            <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                            <div class="breadcrumb-item"><a href="#">Bootstrap Components</a></div>
+                            <div class="breadcrumb-item">Breadcrumb</div>
+                        </div>
                     </div>
 
                     <div id="vueapp" class="section-body">
@@ -39,9 +44,8 @@
             </div>
             <footer class="main-footer">
                 <div class="footer-left">
-                    Copyright &copy; {{\Carbon\Carbon::now()->year}} <div class="bullet"></div> Elasher, bought to you by <a href="https://appsdept.com/"
-                    target="_blank"
-                    >AppsDept</a>
+                    Copyright &copy; {{ \Carbon\Carbon::now()->year }} <div class="bullet"></div> Elasher, bought to you
+                    by <a href="https://appsdept.com/" target="_blank">AppsDept</a>
                 </div>
                 <div class="footer-right">
 
