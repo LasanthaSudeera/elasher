ba328086cd8b6dbc157e640a1dc97766bc5f8197
Lasantha <lasan.sudeera@gmail.com>
2022-11-06 17:00:52 +0530

    EL-3: Added bootstapjs from npm

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -469,8 +469,7 @@
     <script src="assets/modules/jquery.min.js"></script>
     <script src="assets/modules/popper.js"></script>
     <script src="assets/modules/tooltip.js"></script>
-    <script src="assets/modules/bootstrap/js/bootstrap.min.js"></script>
-    {{-- @vite('resources/js/app.js') --}}
+    @vite('resources/js/app.js')
     <script src="assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
     <script src="assets/modules/moment.min.js"></script>
     <script src="assets/js/stisla.js"></script>
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+import * as path from "path";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-                'Modules/Core/Resources/assets/sass/app.scss',
-                'Modules/Core/Resources/assets/js/app.js'
+                "resources/sass/app.scss",
+                "resources/js/app.js",
+                "Modules/Core/Resources/assets/sass/app.scss",
+                "Modules/Core/Resources/assets/js/app.js",
             ],
             refresh: true,
         }),
@@ -24,7 +25,8 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
+            vue: "vue/dist/vue.esm-bundler.js",
+            "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
         },
     },
 });
