dfd32dce9689a64b76f776f24927bf7c0c0e47b8
Lasantha <lasan.sudeera@gmail.com>
2022-11-07 18:37:46 +0530

    EL-7: Implementing new login ui

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,8 @@ class User extends Authenticatable
         'password',
         'first_name',
         'last_name',
-        'username'
+        'username',
+        'password'
     ];
 
     /**
@@ -43,4 +44,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Mutator to bcrypt the password by default
+     *
+     * @param [string] $password
+     * @return void
+     */
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
 }
