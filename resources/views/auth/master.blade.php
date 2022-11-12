eda67a0c636ace5af1c39dd10d6fb41dd56cedc2
Lasantha <lasan.sudeera@gmail.com>
2022-11-12 11:06:33 +0530

    EL-8: Minor fix

--- resources/views/auth/master.blade.php
+++ resources/views/auth/master.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Login &mdash; Elasher</title>
+    <title>{{$pageName}} &mdash; Elasher</title>
 
     <!-- General CSS Files -->
     @vite('resources/sass/app.scss')
