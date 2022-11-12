ceb18d9f825ccb9c8d4124d625162d37e5bf7f5a
Lasantha <lasan.sudeera@gmail.com>
2022-11-12 10:59:05 +0530

    EL-8: Minor code refactor

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,118 +1,78 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('auth.master')
 
-<head>
-    <meta charset="UTF-8">
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Login &mdash; Elasher</title>
+@section('content')
+    <section class="section">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                    <div class="login-brand">
+                        <img src="{{ Avatar::create('E L')->toBase64() }}" alt="logo" width="100"
+                            class="shadow-light rounded-circle">
+                    </div>
 
-    <!-- General CSS Files -->
-    @vite('resources/sass/app.scss')
-    <link rel="stylesheet" href="/assets/modules/fontawesome/css/all.min.css">
-
-    <!-- CSS Libraries -->
-    <link rel="stylesheet" href="/assets/modules/bootstrap-social/bootstrap-social.css">
-
-    <!-- Template CSS -->
-    <link rel="stylesheet" href="/assets/css/style.css">
-    <link rel="stylesheet" href="/assets/css/components.css">
-</head>
-
-<body>
-    <div id="app">
-        <section class="section">
-            <div class="container mt-5">
-                <div class="row">
-                    <div
-                        class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-                        <div class="login-brand">
-                            <img src="{{ Avatar::create('E L')->toBase64() }}" alt="logo" width="100"
-                                class="shadow-light rounded-circle">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h4>Login</h4>
                         </div>
 
-                        <div class="card card-primary">
-                            <div class="card-header">
-                                <h4>Login</h4>
-                            </div>
+                        <div class="card-body">
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input value="{{ old('email') }}" id="email" type="email" class="form-control"
+                                        name="email" tabindex="1" required autofocus>
+                                    @error('email')
+                                        <span class="invalid-feedback d-block" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
 
-                            <div class="card-body">
-                                <form method="POST" action="{{ route('login') }}">
-                                    @csrf
-                                    <div class="form-group">
-                                        <label for="email">Email</label>
-                                        <input value="{{ old('email') }}" id="email" type="email"
-                                            class="form-control" name="email" tabindex="1" required autofocus>
-                                        @error('email')
-                                            <span class="invalid-feedback d-block" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
+                                <div class="form-group">
+                                    <div class="d-block">
+                                        <label for="password" class="control-label">Password</label>
+                                        @if (Route::has('password.request'))
+                                            <div class="float-right">
+                                                <a href="{{ route('password.request') }}" class="text-small">
+                                                    Forgot Password?
+                                                </a>
+                                            </div>
+                                        @endif
                                     </div>
+                                    <input id="password" type="password" class="form-control" name="password"
+                                        tabindex="2" required>
+                                    @error('password')
+                                        <span class="invalid-feedback d-block" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
 
-                                    <div class="form-group">
-                                        <div class="d-block">
-                                            <label for="password" class="control-label">Password</label>
-                                            @if (Route::has('password.request'))
-                                                <div class="float-right">
-                                                    <a href="{{ route('password.request') }}" class="text-small">
-                                                        Forgot Password?
-                                                    </a>
-                                                </div>
-                                            @endif
-                                        </div>
-                                        <input id="password" type="password" class="form-control" name="password"
-                                            tabindex="2" required>
-                                        @error('password')
-                                            <span class="invalid-feedback d-block" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
+                                <div class="form-group">
+                                    <div class="custom-control custom-checkbox">
+                                        <input type="checkbox" name="remember" class="custom-control-input" tabindex="3"
+                                            id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                        <label class="custom-control-label" for="remember">Remember Me</label>
                                     </div>
+                                </div>
 
-                                    <div class="form-group">
-                                        <div class="custom-control custom-checkbox">
-                                            <input type="checkbox" name="remember" class="custom-control-input"
-                                                tabindex="3" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                            <label class="custom-control-label" for="remember">Remember Me</label>
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
-                                            Login
-                                        </button>
-                                    </div>
-                                </form>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                        Login
+                                    </button>
+                                </div>
+                            </form>
 
 
-                            </div>
                         </div>
+                    </div>
 
-                        <div class="simple-footer">
-                            Copyright &copy; Elasher {{ \Carbon\Carbon::now()->year }}
-                        </div>
+                    <div class="simple-footer">
+                        Copyright &copy; Elasher {{ \Carbon\Carbon::now()->year }}
                     </div>
                 </div>
             </div>
-        </section>
-    </div>
-
-    <!-- General JS Scripts -->
-    <script src="/assets/modules/jquery.min.js"></script>
-    <script src="/assets/modules/popper.js"></script>
-    <script src="/assets/modules/tooltip.js"></script>
-    @vite('resources/js/app.js')
-    <script src="/assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
-    <script src="/assets/modules/moment.min.js"></script>
-    <script src="/assets/js/stisla.js"></script>
-
-    <!-- JS Libraies -->
-
-    <!-- Page Specific JS File -->
-
-    <!-- Template JS File -->
-    <script src="/assets/js/scripts.js"></script>
-    <script src="/assets/js/custom.js"></script>
-</body>
-
-</html>
+        </div>
+    </section>
+@endsection
---
+++ resources/views/auth/master.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>Login &mdash; Elasher</title>
+
+    <!-- General CSS Files -->
+    @vite('resources/sass/app.scss')
+    <link rel="stylesheet" href="/assets/modules/fontawesome/css/all.min.css">
+
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="/assets/modules/bootstrap-social/bootstrap-social.css">
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/components.css">
+</head>
+
+<body>
+    <div id="app">
+        @yield('content')
+    </div>
+
+    <!-- General JS Scripts -->
+    <script src="/assets/modules/jquery.min.js"></script>
+    <script src="/assets/modules/popper.js"></script>
+    <script src="/assets/modules/tooltip.js"></script>
+    @vite('resources/js/app.js')
+    <script src="/assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
+    <script src="/assets/modules/moment.min.js"></script>
+    <script src="/assets/js/stisla.js"></script>
+
+    <!-- JS Libraies -->
+
+    <!-- Page Specific JS File -->
+
+    <!-- Template JS File -->
+    <script src="/assets/js/scripts.js"></script>
+    <script src="/assets/js/custom.js"></script>
+</body>
+
+</html>
